887628ae393ea759b8f7fc3eb93594e2a4bc9a25
ohcaptainmyycaptain <luckylie@proton.me>
2026-05-14 15:58:53 +0200

    index.html hinzugefügt

---
+++ script.js
@@ -0,0 +1,15 @@
+// so fängt es oft an:
+function begruessung() {
+    // 1 wert aus dem eingabefeld holen
+    let name = document.getElementById("nameInput").value;
+ // 2. text mit dr id schreiben
+    document.getElementById("greeting").textContent = "helloo " + name + "!!!!"; 
+}
+
+function myFunction() {
+        alert("button was clicked!!!!1111!!!!!11");
+      }
+
+function kys() {
+    alert("loading request....");
+}
