714050ff0852057ab1df44f0b0bc4c02b15fb766
ohcaptainmyycaptain <luckylie@proton.me>
2026-05-15 03:34:40 +0200

    Store user name in local storage
    Added functionality to store user name in local storage.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function begruessung() {
     // 1 wert aus dem eingabefeld holen
     let name = document.getElementById("nameInput").value;
+// Speichert den Namen im Browser-Speicher
+    localStorage.setItem("user_name", name);
  // 2. text mit dr id schreiben
     document.getElementById("greeting").textContent = "helloo " + name + "!!!!"; 
 }
